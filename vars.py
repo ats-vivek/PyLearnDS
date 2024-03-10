18c4b5f5311ed80c901f14c0351af3787b1b094a
Vivek Trivedi <32205003+trivik19780@users.noreply.github.com>
2024-03-10 12:35:32 +0530

    renamed hospital to input.py made change in vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,13 @@
-price=10
-rating=4.9
-name="Vivek"
-is_pub=False
-print(price)
+#Variable: A named memory address
+price=10        # an integer variable
+rating=4.9      # a flot value
+name="Vivek"    # a string/text valaue
+is_pub=False    # a logical value
+print(price)    # show the value of variable
+print(type(price))
+print(rating)    # show the value of variable
+print(type(rating))
+print(name)    # show the value of variable
+print(type(name))
+print(is_pub)    # show the value of variable
+print(type(is_pub))
