6d7af0b24364b1f330f22b3dccbfc5a665dcc3c1
Vivek Trivedi <32205003+trivik19780@users.noreply.github.com>
2025-03-05 12:28:26 +0530

    concat_input

---
+++ input_func.py
@@ -0,0 +1,3 @@
+ename = input("Enter your name: ")
+print("Hello", ename)
+print("hey " + ename +" ,how are you?")
---
+++ lec2_concat.py
@@ -0,0 +1,3 @@
+print("hello world \nhello world\nhello world")
+print("hello" + "vivek")
+print("hello \t" + "vivek")
