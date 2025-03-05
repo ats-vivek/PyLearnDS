812601437534cc2fd63fc61083cd1f3086cc89e1
Vivek Trivedi <32205003+trivik19780@users.noreply.github.com>
2025-03-05 12:37:22 +0530

    swapnum

---
+++ Ex1_Swapnum.py
@@ -0,0 +1,12 @@
+n1=input("Enter first number: ")
+n2=input("Enter second number: ")
+t1= n1
+n1=n2
+n2=t1
+print("First number is: ", n1)
+print("Second number is: ", n2)
+# Expected Output:
+# Enter first number: 10
+# Enter second number: 20
+# First number is: 20
+# Second number is: 10
